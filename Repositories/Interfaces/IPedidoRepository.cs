4ff6ef13870f2628c4059766bb602a2335e935a9
vitim1aguilar <vitim1aguilar@gmail.com>
2025-06-08 13:27:45 -0300

    define as interfaces que cada repositório deve seguir, e serve para garantir que o código fique desacoplado e possa ser facilmente substituído no futuro

--- Repositories/Interfaces/IPedidoRepository.cs
+++ Repositories/Interfaces/IPedidoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P2POO.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace P2POO.Interfaces
 {
     interface IPedidoRepository
     {
+        void Adicionar(Pedido pedido);
+        IEnumerable<Pedido> ObterTodos();
     }
 }
--- Repositories/Interfaces/IProdutoRepository.cs
+++ Repositories/Interfaces/IProdutoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P2POO.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace P2POO.Interfaces
 {
     interface IProdutoRepository
     {
+        void Adicionar(Produto produto);
+        IEnumerable<Produto> ObterTodos();
+        Produto? ObterPorId(Guid id);
     }
 }
+
