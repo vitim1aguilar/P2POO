dfe51fb6382d536e31555938fc6f93f7f39f02c8
vitim1aguilar <vitim1aguilar@gmail.com>
2025-06-08 13:20:30 -0300

    ajustando as pastas

--- Repositories/Interfaces/IProdutoRepository.cs
+++ Repositories/Interfaces/IProdutoRepository.cs
@@ -1,16 +1,12 @@
-﻿using P2POO.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace P2POO.Repositories.Interfaces
+namespace P2POO.Interfaces
 {
-    public interface IProdutoRepository
+    interface IProdutoRepository
     {
-        void Adicionar(Produto produto);
-        IEnumerable<Produto> ObterTodos();
-        Produto? ObterPorId(Guid id);
     }
 }
