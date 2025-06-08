f84d35f7dd3b6af36c3ceec0a9dc262e25c8cfe5
vitim1aguilar <vitim1aguilar@gmail.com>
2025-06-08 15:20:58 -0300

    aplica um desconto baseado na quantidade comprada exemplo: 5% para 5 ou mais unidades e tambem é uma implementação concreta de IDiscountStrategy

--- Discount/QuantidadeDiscountStrategy.cs
+++ Discount/QuantidadeDiscountStrategy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P2POO.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace P2POO.Discount
 {
-    class QuantidadeDiscountStrategy
+    public class QuantidadeDiscountStrategy : IDiscountStrategy
     {
+        public decimal CalcularDesconto(Pedido pedido)
+        {
+            var desconto = 0m;
+
+            foreach (var item in pedido.Itens)
+            {
+                if (item.Quantidade >= 5)
+                {
+                    desconto += item.ValorTotal * 0.05m;
+                }
+            }
+
+            return desconto;
+        }
     }
 }
