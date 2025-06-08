d0aa1704c94dadf9793a4e863289d7f116b28e89
vitim1aguilar <vitim1aguilar@gmail.com>
2025-06-08 14:33:14 -0300

    implementa o padrão de projeto factory, centraliza a lógica de criação dos novos pedidos, garantindo que eles sejam criados corretamente e sempre com uma lista de itens válida

--- Factories/PedidoFactory.cs
+++ Factories/PedidoFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P2POO.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace P2POO.Factories
 {
-    class PedidoFactory
+    public class PedidoFactory
     {
+        public Pedido CriarPedido(Cliente cliente, List<ItemPedido> itens)
+        {
+            return new Pedido(cliente, itens);
+        }
     }
 }
