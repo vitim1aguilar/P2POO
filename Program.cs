2d393472bf42223070ca9e4ea74042a1b45b72c2
vitim1aguilar <vitim1aguilar@gmail.com>
2025-06-08 19:39:36 -0300

    Ponto de entrada da aplicação,onde acontece: Cadastro de produtos e clientes, criação de pedidos, aplicação de descontos, geração de relatórios no console e o uso do serviço de log para registrar cada operação.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using P2POO.Models;
+using P2POO.Repositories;
+using P2POO.Services.Logging;
+using P2POO.Factories;
+using P2POO.Services.Discount;
+
+var logger = new ConsoleLogger();
+
+var produtoRepo = new ProdutoRepository();
+var clienteRepo = new ClienteRepository();
+var pedidoRepo = new PedidoRepository();
+var pedidoFactory = new PedidoFactory();
+
+var descontoCategoria = new CategoriaDiscountStrategy();
+var descontoQuantidade = new QuantidadeDiscountStrategy();
+
+// simulando os dados
+var cliente = new Cliente("Victor Aguilar", "victor@gmail.com", "40028922");
+clienteRepo.Adicionar(cliente);
+
+var notebook = new Produto("Notebook", 4500, "Eletrônico");
+var livro = new Produto("Dom Casmurro", 30, "Livro");
+
+produtoRepo.Adicionar(notebook);
+produtoRepo.Adicionar(livro);
+
+var itens = new List<ItemPedido>
+{
+    new ItemPedido(notebook, 1),
+    new ItemPedido(livro, 1)
+};
+
+var pedido = pedidoFactory.CriarPedido(cliente, itens);
+pedidoRepo.Adicionar(pedido);
+
+logger.Log("Pedido criado com sucesso");
+
+// calcular desconto
+var descontoTotal = descontoCategoria.CalcularDesconto(pedido) +
+                    descontoQuantidade.CalcularDesconto(pedido);
+
+Console.WriteLine("===== Relatório de Pedidos =====");
+foreach (var p in pedidoRepo.ObterTodos())
+{
+    Console.WriteLine($"\nPedido: {p.Id}");
+    Console.WriteLine($"Cliente: {p.Cliente.Nome}");
+    Console.WriteLine($"Data: {p.Data}");
+
+    foreach (var item in p.Itens)
+    {
+        Console.WriteLine($" - {item.Produto.Nome} x{item.Quantidade} - R$ {item.ValorTotal}");
+    }
+
+    Console.WriteLine($"Subtotal: R$ {p.ValorTotal}");
+    Console.WriteLine($"Desconto aplicado: R$ {descontoTotal}");
+    Console.WriteLine($"Total Fonal: R$ {p.ValorTotal - descontoTotal}");
+}
+
+logger.Log("Relatório gerado.");
