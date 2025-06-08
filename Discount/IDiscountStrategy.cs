162201b85ed4ec698adc67762989e79e8e2901a3
vitim1aguilar <vitim1aguilar@gmail.com>
2025-06-08 14:38:56 -0300

    isso permite que diferentes tipos de desconto sejam aplicados sem precisar mudar a lógica do Pedido

--- Discount/IDiscountStrategy.cs
+++ Discount/IDiscountStrategy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P2POO.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace P2POO.Discount
 {
-    interface IDiscountStrategy
+    public interface IDiscountStrategy
     {
+        decimal CalcularDesconto(Pedido pedido);
     }
 }
